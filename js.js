a51cecaad3e11bb76fbc52c4f784057537f2a52e
Joseph Nguyen <josephnguyen2@gmail.com>
2021-03-30 00:54:49 -0700

    .

---
+++ js.js
@@ -0,0 +1,15 @@
+var pageGreeting = document.getElementById("pageGreeting");
+var popup = document.getElementById("popup");
+var clock = document.getElementById("clock")
+function signIn() {
+    var userName = document.getElementById("myInput").value;
+    popup.innerHTML = ('<h1>' + userName + '</h1>');
+    
+}
+
+window.onload = displayClock();
+function displayClock(){
+  var display = new Date().toLocaleTimeString();
+  clock.innerHTML = display;
+  setTimeout(displayClock, 1000); 
+}
